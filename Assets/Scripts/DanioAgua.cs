b771033e62be7ef206a8b94100faba4d27775831
NicolasMCorderoR <nicolascordero.227@gmail.com>
2025-09-12 16:37:25 -0300

    comienzo de desarrollo en Script del daño del agua

--- Assets/Scripts/DanioAgua.cs
+++ Assets/Scripts/DanioAgua.cs
@@ -1,16 +1,40 @@
+using System;
 using UnityEngine;
 
 public class DanioAgua : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private float damage;
+    private float attackspeed;
+    private float attacktimer;
+    private bool isAttacking;
+    GameObject player;
+
+    private void Start()
     {
-        
+        player = GameObject.Find("Player");
+        attacktimer = attackspeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isAttacking)
+        {
+            attacktimer -= Time.deltaTime;
+            if (attacktimer <= 0)
+            {
+                player.transform.SendMessage("takeDamage", damage);
+                attacktimer = attackspeed;
+            }
+        }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player");
+    }
+
 }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
